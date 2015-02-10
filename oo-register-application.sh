975e95f0c6ac67382a2444bffb044bdc5440f171
John Kwiatkoski <jkwiatko@redhat.com>
2015-02-10 16:08:16 -0700

    Updated parameter parsing using getopt and skips invalid users when granting permissions

--- oo-register-application.sh
+++ oo-register-application.sh
@@ -28,6 +28,9 @@ brokerhost="localhost"
 #defaultGearProfile="small"
 logfile=/var/log/openshift/broker/ose-utils.log
 
+#GLOBAL VARIABLES
+force=0
+
 function usage {
   echo "Usage: oo-register-application.sh {gearProfile} {appDomain} [--appAdmins {appAdminsCSV}] [--developers {developersCSV}]"
 }
@@ -74,6 +77,8 @@ function validGear {
 
 }
 
+
+
 if [ "$#" -lt 1 ]
   then
   usage
@@ -81,48 +86,46 @@ if [ "$#" -lt 1 ]
   exit 255
 fi
 
+
 gearProfile="$1"
 appDomain="$2"
+ARGS=`getopt -o h --long "appAdmins:,developers:,force" -n "oo-register-application" -- "$@"`
+shift;shift;
+eval set -- "$ARGS";
+while true; do
+  case "$1" in
+    --appAdmins )
+        case "$2" in
+          "") json 255 "Invalid usage"; shift;;
+          *) appAdmins="$2"; shift ; shift;;
+        esac
+    ;;
+    --developers )
+        case "$2" in
+          "") json 255 "Invalid usage"; shift;;
+          *) developers="$2"; shift; shift;;
+        esac
+    ;;
+    --force )
+        shift;
+        force=1;
+    ;;
+    -h ) usage;;
+    --) shift; break;;
+    *) echo "not recognized";;
 
-if [ -z $2 ];then
-  json 255 "Must provide both gearProfile and appdomain"
-  exit 255
-else
-  checkGear=$(validGear "$gearProfile")
-  if [ "$checkGear" != "1" ];then
-    json 255 "Invalid Gear Size."
-    exit 255
-  fi
+  esac
+done
+
+
+if [[ $appAdmins != [0-9A-Za-z,]* ]] || [[ $developers != [0-9A-Za-z,]* ]];then
+   json 255 "invalid username detected"
+   exit 255
 fi
 
-if [  -z ${appDomain+x} ]
-  then
-  usage
-  json 255 "appDomain not set" ""
-  exit 1
-fi
-if [ -z ${3+x} ]
-  then
-  #no optional parameters provided
-  sleep 0
-elif [ "$3" != "--appAdmins" ] && [ "$3" != "--developers" ]
-    then
-    usage
-    json 255 "Unrecognized flag $3" ""
-    exit 255
-elif [ "$3" = "--appAdmins" ]
-  then
-  appAdminCVS=$4
-else [ "$3" = "--developers" ]
-  developers=$4
-fi
-
-if [ "$5" = "--appAdmins" ]
-  then
-  appAdminCVS=$6
-elif [ "$5" = "--developers" ]
-  then
-  developers=$6
+if [[ ${#appDomain} -gt 16 ]];then #appdomain is longer than 16 error out
+   json 255 "App Domain too long"
+   exit 255
 fi
 
 # #1. Create internal Openshift user "$appDomain"
@@ -139,18 +142,17 @@ if [[ "$code" = "5" ]]; then
 
 elif [[ "$code" = "0" ]]; then
   #already exists do nothing return 1
-  json 1 "User already exists. Exiting..."
-  exit 1
+  echo "Force = $force"
+  if [ $force -ne 1 ]; then
+    json 1 "User already exists. Exiting..."
+    exit 1
+  fi
 else
   #Unknow error
   echo "error=$code"
   json 255 "Unknown Error. Exiting..."
   exit 255;
 fi
-
-
-
-
 
 #Create Token (expires a year from creation
 TOKEN="$(oo-auth-token -l $appDomain -e "$(date -d "+year" 2>> $logfile | tee -a $logfile)" 2>> $logfile | tee -a $logfile)"
@@ -159,9 +161,13 @@ TOKEN="$(oo-auth-token -l $appDomain -e "$(date -d "+year" 2>> $logfile | tee -a
 #2. Create application domain owned by appDomain Openshift user $appDomain
 response=$( curl -k -H "Authorization: Bearer $TOKEN" -X POST https://$brokerhost/broker/rest/domains/ --data-urlencode name=$appDomain --data-urlencode allowed_gear_sizes=small 2>> $logfile | tee -a $logfile 2>> $logfile)
 status=$(exit_code "$response")
-if [ "$status" = 103 ];then
+if [ "$status" = 103 ] && [ "$force" != 1 ];then
     json 1 "Domain already exists. Openshift API exit code $status"
     exit 1
+
+elif [ "$status" = 103 ] && [ "$force" = 1 ];then
+    echo "Forcing changed to existing domain" &>> $logfile
+    
 elif [ "$status" != 0 ];then
      json 255 "Error creating Domain. Openshift API exit code $status"
      exit 255
@@ -169,13 +175,12 @@ fi
 
 
 #4. grant edit access on the application domain to all employeeNums listed under appAdmins
-IFS=',' read -ra admins <<< $appAdminCVS
+IFS=',' read -ra admins <<< $appAdmins
 for employee in ${admins[*]}; do
     response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=admin --data-urlencode login=$employee 2>> $logfile | tee -a $logfile )
     status=$(exit_code "$response")
       if [ "$status" != 0 ];then
-        json 255 "Error granting administrator permissions. Openshift API exit code $status"
-        exit 255
+        echo "Error granting administrator permissions. Openshift API exit code $status" &>>$logfile
     fi
 done
 
@@ -187,9 +192,7 @@ for dev in ${devs[*]}; do
     response=$(curl -k -H "Authorization: Bearer $TOKEN" -X PATCH https://$brokerhost/broker/rest/domains/$appDomain/members --data-urlencode role=view --data-urlencode login=$dev 2>> $logfile| tee -a $logfile )
     status=$(exit_code "$response")
     if [ $status != 0 ];then
-        json 255 "Error granting developer permissions. Openshift API exit code $status"
-        echo "3"
-        exit 255
+        echo "Error granting developer permissions. Openshift API exit code $status" &>>$logfile
     fi
 done
 
