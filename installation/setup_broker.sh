669b27c3d90fd9b702bebcebc51953c94efd4e53
Eric Sauer <esauer@redhat.com>
2015-02-09 21:19:27 -0500

    Fixed bad formatting.

--- installation/setup_broker.sh
+++ installation/setup_broker.sh
@@ -5,7 +5,7 @@ echo "export PS1=\"[\u@\h <broker> \W]\\$ \"" >> ~/.bash_profile
 subscription-manager register --auto-attach
 
 subscription-manager repos --disable="*"
-subscription-manager repos --enable="rhel-6-server-rpms rhel-6-server-ose-2.2-rhc-rpms rhel-6-server-supplementary-rpms rhel-6-server-optional-rpms rhel-server-rhscl-6-rpms rhel-6-server-ose-2.2-infra-rpms"
+subscription-manager repos --enable="rhel-6-server-rpms" --enable="rhel-6-server-ose-2.2-rhc-rpms" --enable="rhel-6-server-supplementary-rpms" --enable="rhel-6-server-optional-rpms" --enable="rhel-server-rhscl-6-rpms" --enable="rhel-6-server-ose-2.2-infra-rpms"
 
 yum -y update
 
