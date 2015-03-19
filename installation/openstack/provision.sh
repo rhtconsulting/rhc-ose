804fc01bea142677aca02efa4320eda74538eaea
Eric Sauer <esauer@redhat.com>
2015-03-19 17:23:45 -0400

    Fixing debug output in provisioning script

--- installation/openstack/provision.sh
+++ installation/openstack/provision.sh
@@ -29,6 +29,15 @@ run_cmd_with_timeout() {
   [ $next_wait_time -eq $timeout ] && echo "Command $command timed out after $timeout seconds."
 }
 
+safe_out() {
+  [ "$1" == "debug" ] && [ ! -z $debug ] && echo "$1: $2"
+  [ "$1" == "info" ] && [ ! -z $interactive ] && echo "$1: $2"
+  [ "$1" == "error" ] && echo "$1: $2"
+}
+
+# Initialize environment
+interactive="true"
+
 # Process options
 while [[ $# -gt 0 ]] &&  [[ ."$1" = .--* ]] ;
 do
@@ -39,13 +48,15 @@ do
     "--key" )
       key="$1"; shift;;
     "--n")
-      interactive="false";;
+      unset interactive;;
     "--instance-name")
       instance_name="$1"; shift;;
     "--rhel7")
       image_name="rhel-guest-image-7.0-20140618.1";;
     "--auth-key-file")
       options="${options} --file /root/.ssh/authorized_keys=$1"; shift;;
+    "--debug")
+      debug=1;;
     *) echo >&2 "Invalid option: $@"; exit 1;;
   esac
 done
@@ -60,25 +71,25 @@ fi
 openstack_cred=${OPENSTACK_CRED_HOME:-~/.openstack/openrc.sh}
 image_name_search=${image_name:-"rhel-guest-image-6.5-20140603.0"}
 rc_file="${openstack_cred}"
-interactive="true"
 #security_groups="default,osebroker,osenode"
 security_groups="default"
 flavor="m1.large"
 #num_of_brokers=1
 #num_of_nodes=1
 if [ ! -f $rc_file ]; then
-  echo "OpenStack API Credentials not found. Default location is ${rc_file}, or set OPENSTACK_CRED_HOME."
+  safe_out "error" "OpenStack API Credentials not found. Default location is ${rc_file}, or set OPENSTACK_CRED_HOME."
   exit 1
 fi
 
 if [ -z $security_groups ]; then
   options="${options} --security-groups ${security_groups}"
+fi
 
 . $rc_file
 
 # Provision VMs
 image_ami=$(nova image-list | awk "/$image_name_search/"'{print $2}')
-echo "nova boot --image ${image_ami} --flavor ${flavor} --key-name ${key} ${options} ${instance_name}"
+safe_out "debug" "nova boot --image ${image_ami} --flavor ${flavor} --key-name ${key} ${options} ${instance_name}"
 status=$(nova boot --image ${image_ami} --flavor ${flavor} --key-name ${key} ${options} ${instance_name} | awk '/status/ {print $4}')
 if [ "$status" != "BUILD" ]; then
   echo "Something went wrong during image creation."
@@ -87,16 +98,12 @@ if [ "$status" != "BUILD" ]; then
   exit 1
 fi
 
-if [ "$interactive" = "true" ]; then
-  echo "Instance ${instance_name} created. Waiting for instance to start..."
-fi
+safe_out "info" "Instance ${instance_name} created. Waiting for instance to start..."
 
 # need to wait for instance to be in running state
 wait_for_instance_running $instance_name
 
-if [ "$interactive" = "true" ]; then
-  echo "Instance ${instance_name} is active. Waiting for ssh service to be ready..."
-fi
+safe_out "info" "Instance ${instance_name} is active. Waiting for ssh service to be ready..."
 
 instance_ip=$(nova show $instance_name | awk '/os1-internal.*network/ {print $6}')
 
