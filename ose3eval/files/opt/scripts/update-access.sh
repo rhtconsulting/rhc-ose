7b31174c56fedcece30c5c9238ae52b5101107df
Eric Sauer <esauer@redhat.com>
2015-06-10 15:53:02 -0400

    This job will sync our ssh keys for the team

---
+++ ose3eval/files/opt/scripts/update-access.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+AUTHORIZED_KEYS_FILE='https://raw.githubusercontent.com/redhat-consulting/ose-utils/openshift-enterprise-3/ose3eval/ose3_public_keys'
+
+# Usage: added_lines oldfile newfile
+added_lines() {
+  diff --changed-group-format='%>' --unchanged-group-format='' $1 $2
+}
+
+pushd ~ > /dev/null
+  curl -o public_keys.tmp -sS ${AUTHORIZED_KEYS_FILE}
+  echo "$(added_lines .ssh/authorized_keys public_keys.tmp)" >> .ssh/authorized_keys
+  rm public_keys.tmp
+popd
