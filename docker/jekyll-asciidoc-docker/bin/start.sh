bc46a160aa32235cf2675bf778f473d0bfdd2761
Andrew Block <andy.block@gmail.com>
2016-03-29 19:32:18 -0400

    Upgrades jekyll asciidoc builder image

--- docker/jekyll-asciidoc-docker/bin/start.sh
+++ docker/jekyll-asciidoc-docker/bin/start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 SOURCE_DIR=/home/builder/source
+BUILD_DIR=/home/builder/build
 
 if [ ! -d "$SOURCE_DIR" ]; then
    echo "Error: Source volume not mounted or available"
    exit 1
 fi
 
-cd "$SOURCE_DIR"
+mkdir -p "${BUILD_DIR}"
+
+cp -a ${SOURCE_DIR}/. ${BUILD_DIR}
+
+cd "$BUILD_DIR"
 
 bundle install
 bundle exec jekyll serve --host=0.0.0.0
