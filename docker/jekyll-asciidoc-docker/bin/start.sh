bfc0bb40157388e12d40d62990ff897fc9cb5bbc
Andrew Block <andy.block@gmail.com>
2016-03-31 14:37:06 -0400

    Revert changes to jekyll docker container to restore live preview and site generation locally

--- docker/jekyll-asciidoc-docker/bin/start.sh
+++ docker/jekyll-asciidoc-docker/bin/start.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 SOURCE_DIR=/home/builder/source
-BUILD_DIR=/home/builder/build
 
 if [ ! -d "$SOURCE_DIR" ]; then
    echo "Error: Source volume not mounted or available"
    exit 1
 fi
 
-mkdir -p "${BUILD_DIR}"
-
-cp -a ${SOURCE_DIR}/. ${BUILD_DIR}
-
-cd "$BUILD_DIR"
+cd "$SOURCE_DIR"
 
 bundle install
 bundle exec jekyll serve --host=0.0.0.0
