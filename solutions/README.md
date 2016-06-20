54002a6a20dead35cb95b8f5bc19f14d48c25cf2
Andrew Block <andy.block@gmail.com>
2016-06-20 09:23:52 -0400

    Creation of solutions folder including script to associate openshift default signer to metrics and logging

---
+++ solutions/README.md
@@ -0,0 +1,4 @@
+ose3 Solutions
+=============
+
+Solutions for common use cases during the installation and operation of OpenShift Enterprise 3
---
+++ solutions/infra_svcs_certificates/certificates-secrets-metrics-logging.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# Script to automate the creation of certificates and a secret for the Metrics and Logging Deployers
+#
+
+OSE_MASTER_ETC_HOME="/etc/origin/master"
+
+usage() {
+  echo "
+  Usage: $0 [options]
+
+  Options:
+  -h|--hostname=<hostname>                  : Hostname(s) for pass into the certificate generation
+  -t|--type=<logging|metrics>               : Type of operation to perform (Logging or Metrics)
+  -p|--project=<project>                    : Overrides the default project name for the type"
+}
+
+
+# Process input
+for i in "$@"
+do
+  case $i in
+    -h=*|--hostname=*)
+      HOSTNAME="${i#*=}"
+      shift;;
+    -t=*|--type=*)
+      TYPE="${i#*=}"
+      shift;;
+    -p=*|--project=*)
+      TYPE="${i#*=}"
+      shift;;
+      *)
+      echo "Invalid Option: ${i#*=}"
+      usage
+      exit 1;
+      ;;
+  esac
+done
+
+
+function do_logging() {
+    
+    LOGGING_PROJECT=${PROJECT:-logging}
+    
+    oadm ca create-server-cert --key=$OSE_MASTER_ETC_HOME/kibana.key --cert=$OSE_MASTER_ETC_HOME/kibana.crt --hostnames=kibana,$HOSTNAME --signer-cert=$OSE_MASTER_ETC_HOME/ca.crt --signer-key=$OSE_MASTER_ETC_HOME/ca.key --signer-serial=$OSE_MASTER_ETC_HOME/ca.serial.txt
+    
+    oc secrets new logging-deployer kibana.crt=$OSE_MASTER_ETC_HOME/kibana.crt kibana.key=$OSE_MASTER_ETC_HOME/kibana.key kibana-ops.crt=$OSE_MASTER_ETC_HOME/kibana.crt kibana-ops.key=$OSE_MASTER_ETC_HOME/kibana.key ca.crt=$OSE_MASTER_ETC_HOME/ca.crt ca.key=$OSE_MASTER_ETC_HOME/ca.key -n $LOGGING_PROJECT
+
+}
+
+function do_metrics() {
+    
+    METRICS_PROJECT=${PROJECT:-openshift-infra}
+    
+    oadm ca create-server-cert --key=$OSE_MASTER_ETC_HOME/hawkular-metrics.key --cert=$OSE_MASTER_ETC_HOME/hawkular-metrics.crt --hostnames=hawkular-metrics,$HOSTNAME --signer-cert=$OSE_MASTER_ETC_HOME/ca.crt --signer-key=$OSE_MASTER_ETC_HOME/ca.key --signer-serial=$OSE_MASTER_ETC_HOME/ca.serial.txt
+    cat $OSE_MASTER_ETC_HOME/hawkular-metrics.key $OSE_MASTER_ETC_HOME/hawkular-metrics.crt > $OSE_MASTER_ETC_HOME/hawkular-metrics.pem
+
+    oc secrets new metrics-deployer hawkular-metrics.pem=$OSE_MASTER_ETC_HOME/hawkular-metrics.pem hawkular-metrics-ca.cert=$OSE_MASTER_ETC_HOME/ca.crt -n $METRICS_PROJECT
+
+}
+
+
+# Validation Checks
+if [ -z "$HOSTNAME" ]; then
+    echo "Error: Metrics Hostname must be provided"
+    exit 1
+fi
+
+
+if [ ! -d /etc/origin/master ]; then
+    echo "Error: Unable to locate OpenShift Master Directory"
+    exit 1
+fi
+
+if [ "$TYPE" != "logging" ] && [ "$TYPE" != "metrics" ]; then
+    echo "Error: Invalid Type Specifed. Must enter 'logging' or 'metrics'"
+    exit 1
+fi
+
+# Execute function
+echo "About to execute function $TYPE"
+do_$TYPE
+
