8d2d37d4517c9f3e5aec874e6b6530303879c2bb
Andrew Block <andy.block@gmail.com>
2016-06-21 08:25:20 -0400

    Added license and error handling to script file and enhanced solution README

--- solutions/infra_svcs_certificates/certificates-secrets-metrics-logging.sh
+++ solutions/infra_svcs_certificates/certificates-secrets-metrics-logging.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+
 #
-# Script to automate the creation of certificates and a secret for the Metrics and Logging Deployers
+#   Copyright 2016 Red Hat Inc.
 #
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+#
+# certificates-secrets-metrics-logging.sh - Script to automate the creation of certificates and a secret for the Metrics and Logging Deployers
+#
+# Usage: See usage function below
+#
+
+set -e
 
 OSE_MASTER_ETC_HOME="/etc/origin/master"
 
@@ -27,7 +48,7 @@ do
       TYPE="${i#*=}"
       shift;;
     -p=*|--project=*)
-      TYPE="${i#*=}"
+      PROJECT="${i#*=}"
       shift;;
       *)
       echo "Invalid Option: ${i#*=}"
