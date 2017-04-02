f955272efeb187b446580e6a14dabceb1f62ca5f
Steve Ovens <steve_ovens@linux.com>
2017-04-02 13:35:41 -0400

    * Added logging * changed option to dry-run * added option to specify the ocp version * now tracks any failed image download * changed image categories to core_components and hosted_components

--- solutions/disconnected_installs/docker-registry-sync.py
+++ solutions/disconnected_installs/docker-registry-sync.py
@@ -4,6 +4,10 @@ import json
 from pkg_resources import parse_version
 import argparse
 import os
+import subprocess
+import logging
+import datetime
+import time
 
 parser = argparse.ArgumentParser(description='Syncs images from a public docker registry to a private registry. Use '
                                              'this to populate private registries in a closed off environment. Must be '
@@ -13,22 +17,49 @@ parser = argparse.ArgumentParser(description='Syncs images from a public docker 
                                              '--to=<registry-to-sync-to> --file=<image-file>',
                                              formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument('--from', action='store', dest='remote_registry', help='The location of the remote repository')
-parser.add_argument('--local', action='store', dest='local_registry', help='The location of the local repository')
+parser.add_argument('--from', action='store', dest='remote_registry', help='The location of the remote repository',
+                    required=True)
+parser.add_argument('--local', action='store', dest='local_registry', help='The location of the local repository',
+                    required=True)
 parser.add_argument('--file', action='store', dest='json_file', help='A JSON formatted file with the following format:'
                                                                      '{"<tag_type>": {"<namespace>": ["image1", image2"'
-                                                                     'image3]}}')
-parser.add_argument('--output', action='store_true', dest='output', help='If this flag is present, commands will be'
+                                                                     'image3]}}', required=True)
+parser.add_argument('--dry-run', action='store_true', dest='dry_run', help='If this flag is present, commands will be'
                                                                          'dumped to stdout instead of run')
+parser.add_argument('--ocp-version', action='store', dest='ocp_version', help='The version of OpenShift which you '
+                                                                              'want to sync images for')
 
 options = parser.parse_args()
 
+
+# Set up the logger
+todays_date = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d--%H_%M'))
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='/tmp/docker_registry_%s.log' % todays_date,
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+# Set the default release version incase unspecified
 release_version = '3.4'
+
+if options.ocp_version is not None:
+    release_version = options.ocp_version
 
 retrieve_v_tags_from_redhat_list = []
 retrieve_non_v_tags_from_redhat_list = []
 
 latest_tag_list = []
+failed_images = []
+
 
 def generate_url_list(dictionary_key, list_to_populate):
     for namespace in config_file_dict[dictionary_key]:
@@ -36,7 +67,8 @@ def generate_url_list(dictionary_key, list_to_populate):
             docker_json_link = "https://registry.access.redhat.com/v2/%s/%s/tags/list" % (namespace, image)
             list_to_populate.append(docker_json_link)
 
-def get_latest_tag_from_api(url_list, tag_list, version_type = None):
+
+def get_latest_tag_from_api(url_list, tag_list, failed_image_list, version_type = None):
     for url in url_list:
         redhat_registry = urllib.urlopen(url)
         # The object is returned as a string so it needs to be converted to a json object
@@ -65,31 +97,65 @@ def get_latest_tag_from_api(url_list, tag_list, version_type = None):
                         latest_tag = tag
         # We want to remove everything after the hyphen because we don't care about release versions
         latest_tag_minus_hyphon = latest_tag.split('-')[0]
-        tag_list.append("%s:%s" % (image_name, latest_tag_minus_hyphon))
+        if type(latest_tag_minus_hyphon) is not unicode:
+            logging.error("Unable to properly parse the version for image: %s" % image_name)
+            logging.error("Are you sure that the version exists in the RedHat registry?")
+            failed_image_list.append(image_name)
+        else:
+            tag_list.append("%s:%s" % (image_name, latest_tag_minus_hyphon))
+
+
+def generate_realtime_output(cmd):
+    output = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(output.stdout.readline, ""):
+        yield stdout_line.strip()
+    output.stdout.close()
+    return_code = output.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
 
 
 config_file = options.json_file
 with open(config_file) as json_data:
     config_file_dict = json.load(json_data)
 
-generate_url_list('image_with_v_in_tag', retrieve_v_tags_from_redhat_list)
-generate_url_list('images_without_v_in_tag', retrieve_non_v_tags_from_redhat_list)
+generate_url_list('core_components', retrieve_v_tags_from_redhat_list)
+generate_url_list('hosted_components', retrieve_non_v_tags_from_redhat_list)
 
-get_latest_tag_from_api(retrieve_v_tags_from_redhat_list, latest_tag_list)
-get_latest_tag_from_api(retrieve_non_v_tags_from_redhat_list, latest_tag_list, 'v')
+get_latest_tag_from_api(retrieve_v_tags_from_redhat_list, latest_tag_list, failed_images)
+get_latest_tag_from_api(retrieve_non_v_tags_from_redhat_list, latest_tag_list, failed_images, 'v')
 
+total_number_of_images_to_download = len(latest_tag_list)
+counter = 0
+
+logging.info("")
+logging.info("Total images to download: %s" % total_number_of_images_to_download)
 for namespace_and_image in latest_tag_list:
-    if options.output:
+    if options.dry_run:
+        logging.info("Dry run mode activated. Docker commands were outputted to the screen")
         print("docker pull %s/%s" % (options.remote_registry, namespace_and_image))
         print("docker tag %s/%s %s/%s" % (options.remote_registry, namespace_and_image, options.local_registry,
                                           namespace_and_image))
         print("docker push %s/%s" % (options.local_registry, namespace_and_image))
         print("")
     else:
-        print("Pulling %s/%s" % (options.remote_registry, namespace_and_image))
-        os.popen("docker pull %s/%s" % (options.remote_registry, namespace_and_image)).read()
-        print("Tagging for this registry: %s" % options.local_registry)
+        logging.info("")
+        logging.info("Downloading image %s/%s" % (counter, total_number_of_images_to_download))
+        counter += 1
+        logging.info("Pulling %s/%s" % (options.remote_registry, namespace_and_image.strip()))
+        for pulled_image in generate_realtime_output(["docker", "pull", "%s/%s" % (options.remote_registry,
+                                                                                   namespace_and_image)]):
+            logging.info(pulled_image),
+        logging.info("Tagging for this registry: %s" % options.local_registry)
         os.popen("docker tag %s/%s %s/%s" % (options.remote_registry, namespace_and_image, options.local_registry,
                                              namespace_and_image)).read()
-        print("Pushing into the local registry...")
-        os.popen("docker push %s/%s" % (options.local_registry, namespace_and_image)).read()
+        logging.info("Pushing into the local registry...")
+        for pushed_image in generate_realtime_output(["docker", "push", "%s/%s" % (options.local_registry,
+                                                                                   namespace_and_image)]):
+            logging.info(pushed_image),
+
+if failed_images is not None:
+    number_of_failures = len(failed_images)
+    number_of_images_attempted = total_number_of_images_to_download + number_of_failures
+    logging.warn("")
+    logging.warn("%s/%s failed to download: %s" % (number_of_failures, number_of_images_attempted, failed_images))
